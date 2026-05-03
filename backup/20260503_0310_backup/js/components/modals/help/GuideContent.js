875072dfd946b439e2395c5728ae900fd5b3c5a0
leeyj <leeyj78@gmail.com>
2026-05-04 04:03:12 +0900

    chore: remove accidental backup directory

--- backup/20260503_0310_backup/js/components/modals/help/GuideContent.js
+++
@@ -1,9 +0,0 @@
-import { I18nManager } from '../../../utils/I18nManager.js';
-
-export const GuideContent = {
-    async render() {
-        const lang = I18nManager.currentLang || 'ko';
-        const response = await fetch(`/static/html/help/guide_${lang}.html?v=3.0`);
-        return await response.text();
-    }
-};
--- backup/20260503_0310_backup/js/components/modals/help/ShortcutContent.js
+++
@@ -1,9 +0,0 @@
-import { I18nManager } from '../../../utils/I18nManager.js';
-
-export const ShortcutContent = {
-    async render() {
-        const lang = I18nManager.currentLang || 'ko';
-        const response = await fetch(`/static/html/help/shortcuts_${lang}.html?v=3.0`);
-        return await response.text();
-    }
-};
